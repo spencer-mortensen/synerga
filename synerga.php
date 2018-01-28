610e7b24cd3a7c29f7cbe7a52665993ecf0fcc11
Spencer Mortensen <spencer@spencermortensen.com>
2018-01-28 09:20:55 -0500

    Removed the old autoloader

--- synerga.php
+++ synerga.php
@@ -4,36 +4,5 @@ namespace Synerga;
 
 require __DIR__ . '/autoload.php';
 
-call_user_func(function () {
-	$projectDirectory = __DIR__;
-	$vendorDirectory = "{$projectDirectory}/vendor";
-
-	$classes = array(
-		'Synerga' => "{$projectDirectory}/src",
-		'SpencerMortensen\\Html5' => "{$vendorDirectory}/spencer-mortensen/html5/src",
-	);
-
-	foreach ($classes as $namespacePrefix => $libraryPath) {
-		$namespacePrefix .= '\\';
-		$namespacePrefixLength = strlen($namespacePrefix);
-
-		$autoloader = function ($class) use ($namespacePrefix, $namespacePrefixLength, $libraryPath) {
-			if (strncmp($class, $namespacePrefix, $namespacePrefixLength) !== 0) {
-				return;
-			}
-
-			$relativeClassName = substr($class, $namespacePrefixLength);
-			$relativeFilePath = strtr($relativeClassName, '\\', '/') . '.php';
-			$absoluteFilePath = "{$libraryPath}/{$relativeFilePath}";
-
-			if (is_file($absoluteFilePath)) {
-				include $absoluteFilePath;
-			}
-		};
-
-		spl_autoload_register($autoloader);
-	}
-});
-
 $synerga = new Synerga();
 $synerga->run('<:router:>');
